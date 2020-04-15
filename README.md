493524de62d813c28ba33478c5d4fa1c2ab97dc8
Anzola Fabio <anzola.fabio@gmail.com>
2020-04-15 12:19:14 +0200

    day twenty-four
    Added files and updated README.md

--- README.md
+++ README.md
@@ -103,3 +103,6 @@ Link to the Discord-server: https://discord.gg/mQpT3f2
 
 #### Day 23 (2020-04-14):
 - Finished stage 2 of the [Linear Equations Solver](https://hyperskill.org/projects/40?goal=7) project
+
+#### Day 24 (2020-04-15):
+- Finished stage 3 of the [Linear Equations Solver](https://hyperskill.org/projects/40?goal=7) project
--- src/solver/Main.java
+++ src/solver/Main.java
@@ -1,5 +1,7 @@
 package solver;
 
+import java.io.File;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 /**
@@ -47,5 +49,136 @@ public class Main {
         double y = (f - c * d / a) / (e - b * d / a);
         double x = (c - b * y) / a;
         System.out.println(x + " " + y);
+    }
+
+    /**
+     * Stage three of the project
+     */
+    public static void stage3(String[] args) throws Exception {
+        //a11 x1 + a12 x2 + ... + a1n xn = b1
+        //a21 x1 + a22 x2 + ... + a2n xn = b2
+        //...
+        //an1 x1 + an2 x2 + ... + ann xn = bn
+
+        double epsilon = 1.0e-10;
+        File in = null;
+        File out = null;
+        String outName = "";
+
+        //get & process input
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("-in") && i + 1 < args.length) {
+                in = new File(args[i + 1]);
+            }
+
+            if (args[i].equals("-out") && i + 1 < args.length) {
+                outName = args[i + 1];
+                out = new File(outName);
+            }
+        }
+
+        //define the matrix
+        double[][] matrix = null;
+
+        //fill the matrix (possibleException thrown)
+        Scanner sc = new Scanner(in);
+        int order = sc.nextInt();
+        matrix = new double[order][order + 1];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = sc.nextDouble();
+            }
+        }
+
+        //solve equation
+        System.out.println("Start solving the equation.");
+        System.out.println("Rows manipulation:");
+
+        for (int i = 0; i < matrix.length; i++) {
+            if (Math.abs(matrix[i][i] - 0.0) <= epsilon) {
+                for (int j = i + 1; j < matrix[i].length; j++) {
+                    if (!(Math.abs(matrix[j][i] - 0.0) <= epsilon)) {
+
+                        for (int k = i; k < matrix[i].length; k++) {
+                            matrix[i][k] += matrix[j][k];
+                        }
+
+                        System.out.printf("R%d + R%d -> R%d\n", i + 1, j + 1, i + 1);
+
+                        break;
+                    }
+                }
+            }
+
+            if (!(Math.abs(matrix[i][i] - 1) <= epsilon)) {
+                double coefficient = matrix[i][i];
+
+                if (!(Math.abs(coefficient - 0) <= epsilon)) {
+                    System.out.printf("%.1f * R%d -> R%d\n", coefficient, i + 1, i + 1);
+                    for (int j = i; j < matrix[i].length; j++) {
+                        matrix[i][j] /= coefficient;
+                    }
+                }
+            }
+
+            for (int j = i + 1; j < matrix.length; j++) {
+
+                if (Math.abs(matrix[i][i] - 0) <= epsilon) {
+                    throw new IllegalArgumentException();
+                }
+
+                double coefficient = -(matrix[j][i] / matrix[i][i]);
+
+                if (!(Math.abs(coefficient - 0) <= epsilon)) {
+                    System.out.printf("%.1f * R%d + R%d -> R%d\n", coefficient, i + 1, j + 1, j + 1);
+                    for (int k = i; k < matrix[i].length; k++) {
+                        matrix[j][k] += matrix[i][k] * coefficient;
+                    }
+                }
+            }
+        }
+
+        for (int i = matrix.length - 1; i >= 0; i--) {
+            for (int j = i; j > 0; j--) {
+                double coefficient = -matrix[j - 1][i];
+
+                if (!(Math.abs(matrix[i][i] - 0.0) <= epsilon)) {
+                    for (int k = matrix[i].length - 1; k > 0; k--) {
+                        matrix[j - 1][k] += matrix[i][k] * coefficient;
+                    }
+
+                    System.out.printf("%.1f * R%d + R%d -> R%d\n", coefficient, i + 1, j + 1, j + 1);
+                }
+            }
+        }
+
+        //define result array
+        double[] result = new double[matrix.length];
+
+        //fill result array
+        for (int i = 0; i < matrix.length; i++) {
+            result[i] = matrix[i][matrix[i].length - 1];
+        }
+
+        //print the solution
+        System.out.print("The solution is: (");
+        for (int i = 0; i < result.length; i++) {
+            if (i != result.length - 1) {
+                System.out.printf("%.1f, ", result[i]);
+            } else {
+                System.out.printf("%.1f)\n", result[i]);
+            }
+        }
+
+        //print to be saved to file
+        System.out.println("Saved to file " + outName);
+
+        try (PrintWriter pw = new PrintWriter(out)) {
+            for (double v : result) {
+                pw.println(v);
+            }
+
+        }
     }
 }
